5d4878880af5156e50778771dcb99807f9ff09b2
douxiaobo <dxb_1020@icloud.com>
2023-02-03 22:14:52 +0800

    20. Valid Parentheses 2/3/2023

---
+++ 20. Valid Parentheses/ConsoleApp1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> st = new Stack<char>();
+            foreach (char n in s)
+            {
+                switch (n)
+                {
+                    case '(':
+                        st.Push('(');
+                        break;
+                    case ')':
+                        if (st.Count == 0)
+                            return false;
+                        if (st.Peek() != '(')
+                            return false;
+                        else
+                            st.Pop();
+                        break;
+                    case '[':
+                        st.Push('[');
+                        break;
+                    case ']':
+                        if (st.Count == 0)
+                            return false;
+                        if (st.Peek() != '[')
+                            return false;
+                        else
+                            st.Pop();
+                        break;
+                    case '{':
+                        st.Push('{');
+                        break;
+                    case '}':
+                        if (st.Count == 0)
+                            return false;
+                        if (st.Peek() != '{')
+                            return false;
+                        else
+                            st.Pop();
+                        break;
+                }
+            }
+            if (st.Count == 0)
+                return true;
+            else
+                return false;
+        }
+        static void Main(string[] args)
+        {
+            string s1 = "()";
+            Console.WriteLine(IsValid(s1));
+            string s2 = "()[]{}";
+            Console.WriteLine(IsValid(s2));
+            string s3 = "(]";
+            Console.WriteLine(IsValid(s3));
+            string s4 = "([)]";
+            Console.WriteLine(IsValid(s4));
+            string s5 = "{[]}";
+            Console.WriteLine(IsValid(s5));
+            string s6 = "]";
+            Console.WriteLine(IsValid(s6));
+            Console.ReadKey();
+        }
+    }
+}
