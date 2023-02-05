a4f414815b26d5ac965de5963eb412bf65d8947a
douxiaobo <dxb_1020@icloud.com>
2023-02-05 14:42:05 +0800

    58. Length of Last Word 2/5/2023

---
+++ 58. Length of Last Word/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            int index = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    index++;
+                }
+                else
+                {
+                    if (index > 0)
+                        return index;
+                }
+            }
+            return index;
+        }
+        static void Main(string[] args)
+        {
+            string s1 = "Hello World";
+            Console.WriteLine(LengthOfLastWord(s1));
+            string s2 = "   fly me   to   the moon  ";
+            Console.WriteLine(LengthOfLastWord(s2));
+            string s3 = "luffy is still joyboy";
+            Console.WriteLine(LengthOfLastWord(s3));
+            Console.ReadKey();
+        }
+    }
+}
