8da83960c8644b611239f0d52cce3db82ae452b4
douxiaobo <dxb_1020@icloud.com>
2023-02-05 12:30:49 +0800

    169.Majority Element

---
+++ 169. Majority Element/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length / 2];
+        }
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 3, 2, 3 };
+            Console.WriteLine(MajorityElement(nums1));
+            int[] nums2 = { 2, 2, 1, 1, 1, 2, 2 };
+            Console.WriteLine(MajorityElement(nums2));
+            Console.ReadKey();
+        }
+    }
+}
