405ebb9d31d3ada45241c5ad2e2de1654d990d82
douxiaobo <dxb_1020@icloud.com>
2023-02-09 11:15:45 +0800

    283. Move Zeroes 2/9/2023

---
+++ 283. Move Zeroes/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void MoveZeroes(int[] nums)
+        {
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    int tmp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = tmp;
+                    j++;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[] nums1 = { 0, 1, 0, 3, 12 };
+            Print(nums1);
+            MoveZeroes(nums1);
+            Print(nums1);
+            int[] nums2 = { 0 };
+            Print(nums2);
+            MoveZeroes(nums2);
+            Print(nums2);
+            Console.ReadKey();
+        }
+        public static void Print(int[] nums)
+        {
+            foreach (int i in nums) { Console.Write(i + " "); }
+            Console.WriteLine();
+        }
+    }
+}
