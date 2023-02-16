da792b35e40a9ee1115771e03350ea048676bb8c
douxiaobo <dxb_1020@icloud.com>
2023-02-16 15:45:11 +0800

    2/16/2023

---
+++ 191. Number of 1 Bits/ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int HammingWeight(uint n)
+        {
+            int index = 0;
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    index++;
+                }
+                n = n / 2;
+            }
+            return index;
+        }//Runtime 31 ms Beats 23.11% Memory 23.7 MB Beats 48.38%
+        public int HammingWeight1(uint n)
+        {
+            int index = 0;
+            while (n > 0)
+            {
+                if ((n & 1) == 1)       //缺少()，就会报错。没搞清楚&和&&的区别。
+                {
+                    index++;
+                }
+                n = n >> 1;
+            }
+            return index;
+        }//Runtime 25 ms Beats 62.86% Memory 23.7 MB Beats 65.20%
+        //&是按位与运算符（或取地址运算符）
+        //&&是条件与运算符（也叫逻辑与运算符）
+        //&优先级高于&&
+        public int HammingWeight2(uint n)
+        {
+            int index = 0;
+            while (n > 0)
+            {
+                index += (int)(n % 2);
+                n /= 2;
+            }
+            return index;
+        }//Runtime 24 ms Beats 70.41% Memory 23.8 MB Beats 18.97%
+        static void Main(string[] args)
+        {
+            uint n1= 0b00000000000000000000000000001011;
+            Console.WriteLine(Convert.ToString(n1,2));
+            Console.WriteLine(HammingWeight(n1));
+            uint n2= 0b00000000000000000000000010000000;
+            Console.WriteLine(n2);
+            Console.WriteLine(HammingWeight(n2));
+            uint n3= 0b11111111111111111111111111111101;
+            Console.WriteLine(n3);
+            Console.WriteLine(HammingWeight(n3));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 206. Reverse Linked List/ConsoleApp1/Program.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    class LinkedList
+    {
+        Node head;
+        public void ReverseList()
+        {
+            Node prev = null;
+            Node current = head;
+            Node next = null;
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+        }
+        public void PrintList()
+        {
+            Node n = head;
+            while (n != null)
+            {
+                Console.Write(n.data + " ");
+                n = n.next;
+            }
+        }
+    }
+    internal class Program
+    {
+        public static ListNode ReverseList(ListNode head)       //可能不对。
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+            ListNode cur = head.next;
+            ListNode next = null;
+            while (cur != null)
+            {
+                next = cur.next;
+                cur.next = head;
+                head.next = next;
+                cur = next;
+            }
+            return head;
+        }
+        static void Main(string[] args)
+        {
+            ListNode List1 = ListToListNode(new List<int> { 1, 2, 3, 4, 5 });
+            Print(List1);
+            Print(ReverseList(List1));
+            ListNode List2 = ListToListNode(new List<int> { 1, 2 });
+            Print(List2);
+            Print(ReverseList(List2));
+            ListNode List3 = ListToListNode(new List<int> {  });
+            Print(List3);
+            Print(ReverseList(List3));
+            Console.ReadKey();
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+        public static ListNode ListToListNode(List<int> list)
+        {
+            if (list == null)
+            {
+                return null;
+            }
+
+            ListNode head = null, tail = null;
+            foreach (var t in list)
+            {
+                if (head == null)
+                {
+                    head = tail = new ListNode(t);
+                }
+                else
+                {
+                    tail.next = new ListNode(t);
+                    tail = tail.next;
+                }
+            }
+
+            return head;
+        }
+        public static void Print(ListNode head)
+        {
+            for (ListNode cur = head; cur != null; cur = cur.next)
+            {
+                if (cur.next != null)
+                {
+                    Console.Write(cur.val + "->");
+                }
+                else
+                {
+                    Console.Write(cur.val);
+                }
+            }
+            Console.WriteLine();
+        }
+        public ListNode ReverseListfromChatGPT(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+            ListNode prev = null;
+            ListNode current = head;
+            ListNode next = null;
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            head = prev;
+            return head;
+        }
+        public ListNode ReverseList10(ListNode head)    //from Sargerous
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+            ListNode resultNode = null;
+            while (head != null)
+            {
+                resultNode = new ListNode(head.val, resultNode);
+                head = head.next;
+            }
+            return resultNode;
+        }
+        public ListNode ReverseList11(ListNode head)    //from Sakhile_Msibi
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+            ListNode pre = null;
+            ListNode cur = head;
+            ListNode next = null;
+            while (cur != null)
+            {
+                next = cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+            head = pre;
+            return head;
+        }
+        public ListNode ReverseList12(ListNode head)        //luongminhduy
+        {
+            Stack<int> st = new Stack<int>();
+            ListNode temp = head;
+            while (temp != null)
+            {
+                st.Push(temp.val);
+                temp = temp.next;
+            }
+            temp = head;
+            while (temp != null)
+            {
+                temp.val = st.Pop();
+                temp = temp.next;
+            }
+            return head;
+        }
+    }
+}
