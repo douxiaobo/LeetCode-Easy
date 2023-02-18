9aa792ba32d8699c093021835371dce6a6a62646
douxiaobo <dxb_1020@icloud.com>
2023-02-18 11:22:31 +0800

    2/18/2023

---
+++ 141. Linked List Cycle/ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //双指针，List,Dict,Hashtab，共四种方法
+        public static bool HasCycle(ListNode head)
+        {
+            ListNode Quick = head;
+            ListNode Slow = head;
+            while (Quick != null && Quick.next != null)     //缺少考虑
+            {
+                Quick = Quick.next.next;
+                Slow = Slow.next;
+                if (Quick == Slow)      //不能写上.val
+                {
+                    return true;
+                }
+            }
+            return false;
+        }//Runtime 114 ms Beats 39.64% Memory 43.1 MB Beats 44.92%
+        public bool HasCycle1(ListNode head)
+        {
+            if (head == null)
+            {
+                return false;
+            }
+            ListNode Quick = head.next;
+            ListNode Slow = head;
+            while (Quick != Slow)
+            {
+                if (Quick?.next == null)        //不点问号，运行失败。不了解问号是什么用处。
+                {
+                    return false;
+                }
+                Quick = Quick.next.next;
+                Slow = Slow.next;
+            }
+            return true;
+        }//Runtime 106 ms Beats 70.44% Memory 43.1 MB Beats 44.92%
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HasCycle(ArrayToListNode(new int[] { 3, 2, 0, -4 },1)));//LeetCode显示是True，pos=1;在VS显示是False.
+            Console.WriteLine(HasCycle(ArrayToListNode(new int[] { 1, 2 },0)));//LeetCode显示是True,pos=0;
+            Console.WriteLine(HasCycle(ArrayToListNode(new int[] { 1 },-1)));
+            Console.ReadKey();
+        }
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+        public static void PrintList(ListNode head)
+        {
+            ListNode n=head;
+            while (n != null)
+            {
+                Console.Write(n.val + " ");
+                n = n.next;
+            }
+            Console.WriteLine();
+        }
+        public static ListNode ArrayToListNode(int[] Array,int pos)     //晚点研究怎么实现环形链表
+        {
+            if (Array == null)
+            {
+                return null;
+            }
+            ListNode head = null, tail = null;
+            ListNode Temp=null;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (head == null)
+                {
+                    head = tail = new ListNode(Array[i]);
+                    tail = tail.next;
+                }
+                else
+                {
+                    tail = new ListNode(Array[i]);
+                    if (i == pos)
+                    {
+                        Temp = tail;
+                    }
+                    tail = tail.next;
+                    
+                }
+            }
+            if(pos>=0)
+            {
+                tail = Temp;
+            }
+            return head;
+        }
+    }
+}
---
+++ 160. Intersection of Two Linked Lists/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null && headB == null)
+            {
+                return null;
+            }
+            else if (headA != null && headB == null)
+            {
+                return headA;
+            }
+            else if (headA == null && headB != null)
+            {
+                return headB;
+            }
+            else
+            {
+                ListNode LNA = headA;
+                while (LNA != null)
+                {
+                    ListNode LNB = headB;
+                    while (LNB != null)
+                    {
+                        if (LNA == LNB)
+                        {
+                            return LNA;
+                        }
+                        LNB = LNB.next;
+                    }
+                    LNA = LNA.next;
+                }
+            }
+            return null;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
