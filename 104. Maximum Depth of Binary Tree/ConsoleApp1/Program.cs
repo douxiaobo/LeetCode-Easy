f6646ea79b11d5a95ff5b805adcb4e08341d144a
douxiaobo <dxb_1020@icloud.com>
2023-02-27 21:37:35 +0800

    2/27/2023

---
+++ 104. Maximum Depth of Binary Tree/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            else if (root.left == null && root.right == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return Math.Max(MaxDepth(root.left) + 1, MaxDepth(root.right) + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val=0, TreeNode left = null,TreeNode right=null) 
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
---
+++ 344. Reverse String/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void ReverseString(char[] s)
+        {
+            int low = 0, high = s.Length - 1;
+            while (low < high)
+            {
+                char ch = s[low];
+                s[low] = s[high];
+                s[high] = ch;
+                low++;
+                high--;
+            }
+        }//Runtime 236 ms Beats 97.65% Memory 61.6 MB Beats 25.52%
+        static void Main(string[] args)
+        {
+            char[] s1 = { 'H', 'e', 'l', 'l', 'o' };
+            Console.WriteLine(s1);
+            ReverseString(s1);
+            Console.WriteLine(s1);
+            char[] s2 = { 'H', 'a', 'n', 'n', 'a', 'h' };
+            Console.WriteLine(s2);
+            ReverseString(s2);
+            Console.WriteLine(s2);
+            Console.ReadKey();
+        }
+    }
+}
