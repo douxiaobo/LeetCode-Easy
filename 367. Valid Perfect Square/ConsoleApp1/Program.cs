6bce4e3d46f89e9efb1b726e51e3164a7a82bf27
douxiaobo <dxb_1020@icloud.com>
2023-03-02 21:55:55 +0800

    3/2/2023

---
+++ 367. Valid Perfect Square/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            long low = 0, high = num;
+            while (low <= high)
+            {
+                long mid = low + (high - low) / 2;
+                if (mid * mid == num)
+                {
+                    return true;
+                }
+                else if (mid * mid > num)
+                {
+                    high = mid - 1;
+                }
+                else if (mid * mid < num)
+                {
+                    low = mid + 1;
+                }
+            }
+            return false;
+        }//Runtime 42 ms Beats 38.34% Memory 28.4 MB Beats 31.63%
+        static void Main(string[] args)
+        {
+        }
+    }
+}
