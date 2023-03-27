a7ba0763cda04cb6c638a62c997c86119a328d88
douxiaobo <dxb_1020@icloud.com>
2023-03-27 21:04:57 +0800

    3/27/2023

---
+++ 171. Excel Sheet Column Number/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int TitleToNumber1(string columnTitle)
+        {
+            int len = columnTitle.Length - 1;
+            int sum = 0;
+            for (int i = len; i >= 0; i--)
+            {
+                sum += (columnTitle[i] - 'A' + 1) * (int)Math.Pow(26, len - i);
+            }
+            return sum;
+        }//Runtime: 62 ms Beats:83.24% Memory:38.2 MB Beats: 78.69%
+
+        public static int TitleToNumber2(string columnTitle)
+        {
+            int len = columnTitle.Length;
+            int sum = 0;
+            for (int i = 0; i < len; i++)
+            {
+                sum += (columnTitle[i] - 'A' + 1) * (int)Math.Pow(26, len - i - 1);
+            }
+            return sum;
+        }//Runtime:69 ms Beats:55.68% Memory:38.6 MB Beats:43.75%
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TitleToNumber1("A"));
+            Console.WriteLine(TitleToNumber2("AB"));
+            Console.WriteLine(TitleToNumber1("ZY"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 338. Counting Bits/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public int[] CountBits(int n)
+        {
+            int[] result = new int[n + 1];
+            for (int i = 0; i < n + 1; i++)
+            {
+                int count = 0;
+                int a = i;
+                while (a != 0)
+                {
+                    count += a % 2;
+                    a = a / 2;
+                }
+                result[i] = count;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
